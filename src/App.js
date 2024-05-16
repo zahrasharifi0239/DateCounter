ce5441d80520f200b645eea12560518aeff587fc
zahrasharifi0239 <sharifi.zahra0239@gmail.com>
2024-05-16 17:56:43 +0330

    version two

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import "./App.css";
+import "./styles.css";
 
-function App() {
+export default function App() {
 	return (
 		<div className="App">
 			<Counter />
@@ -13,34 +13,53 @@ function Counter() {
 	const [count, setCount] = useState(0);
 	const [step, setStep] = useState(1);
 
-	const date = new Date("May 15 2024");
+	function handleReset() {
+		setCount(0);
+		setStep(1);
+	}
+
+	const date = new Date("june 21 2027");
 	date.setDate(date.getDate() + count);
 
 	return (
 		<div>
 			<div>
-				<button onClick={() => setStep((c) => c - 1)}>-</button>
+				<input
+					type="range"
+					min="0"
+					max="10"
+					value={step}
+					onChange={(e) => setStep(Number(e.target.value))}
+				/>
 				<span>Step: {step}</span>
-				<button onClick={() => setStep((c) => c + 1)}>+</button>
 			</div>
+
 			<div>
 				<button onClick={() => setCount((c) => c - step)}>-</button>
-				<span>Count: {count}</span>
+				<input
+					type="text"
+					value={count}
+					onChange={(e) => setCount(Number(e.target.value))}
+				/>
 				<button onClick={() => setCount((c) => c + step)}>+</button>
 			</div>
 
 			<p>
 				<span>
 					{count === 0
-						? "Today is"
+						? "Today is "
 						: count > 0
-						? `${count} days from today is`
-						: `${Math.abs(count)} days ago was`}
+						? `${count} days from today is `
+						: `${Math.abs(count)} days ago was `}
 				</span>
 				<span>{date.toDateString()}</span>
 			</p>
+
+			{count !== 0 || step !== 1 ? (
+				<div>
+					<button onClick={handleReset}>Reset</button>
+				</div>
+			) : null}
 		</div>
 	);
 }
-
-export default App;
